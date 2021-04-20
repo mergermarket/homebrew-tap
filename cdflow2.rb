39e660541d4c5634e3075605a98f3290ef9b955c
cdflow2 publish action <platform@acuris.com>
2021-04-20 12:32:29 +0100

    Update cdflow2 to v0.45.1

--- cdflow2.rb
+++ cdflow2.rb
@@ -1,8 +1,8 @@
 class Cdflow2 < Formula
   desc     "Deployment tooling for continuous delivery"
   homepage "https://github.com/mergermarket/cdflow2"
-  version  ""
-  url      "https://github.com/mergermarket/cdflow2/releases/download//cdflow2-darwin-amd64"
+  version  "v0.45.1"
+  url      "https://github.com/mergermarket/cdflow2/releases/download/v0.45.1/cdflow2-darwin-amd64"
   sha256   ""
   
   def install

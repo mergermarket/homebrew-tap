4cc93118e6f5527f62e2133c3544a1783eedd17d
cdflow2 publish action <platform@acuris.com>
2026-09-17 11:37:10 +0000

    Update cdflow2 to v0.70.5

--- cdflow2.rb
+++ cdflow2.rb
@@ -1,15 +1,15 @@
 class Cdflow2 < Formula
   desc     "Deployment tooling for continuous delivery"
   homepage "https://github.com/mergermarket/cdflow2"
-  version  "v0.70.4"
+  version  "v0.70.5"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/mergermarket/cdflow2/releases/download/v0.70.4/cdflow2-darwin-amd64"
-      sha256 "a42b3272fa602e2339ec92f5f4692f7dc49413ae090cb4cf716256901fbf911d"
+      url "https://github.com/mergermarket/cdflow2/releases/download/v0.70.5/cdflow2-darwin-amd64"
+      sha256 "de9b8977d07a4efd02d068920d559214062f5e712bea32469044644a33247994"
     elsif Hardware::CPU.arm?
-      url "https://github.com/mergermarket/cdflow2/releases/download/v0.70.4/cdflow2-darwin-arm64"
-      sha256 "361e0bff8cf4c7f77e26e6a1b4fbd4ee33e59c211b27b0f3a3dd5efc898b00ca"
+      url "https://github.com/mergermarket/cdflow2/releases/download/v0.70.5/cdflow2-darwin-arm64"
+      sha256 "8dc2d45fffe4eb6432642cabc187168188c76e2b530fc966cec63d36a99d0c30"
     end
   end
   
